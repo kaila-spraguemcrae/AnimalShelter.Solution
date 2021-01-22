5d03497895734a758e3a2ead4bd87b464c010b17
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-22 11:42:46 -0800

    update startup to handle JWT

---
+++ PortlandAnimalShelterApi/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using PortlandAnimalShelterApi.Services;
+using PortlandAnimalShelterApi.Entities;
+
+namespace PortlandAnimalShelterApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]User userParam)
+        {
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+            return Ok(user);
+        }
+
+        [Authorize(Roles = Role.Admin)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users =  _userService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user =  _userService.GetById(id);
+
+            if (user == null) {
+                return NotFound();
+            }
+
+            var currentUserId = int.Parse(User.Identity.Name);
+            if (id != currentUserId && !User.IsInRole(Role.Admin)) {
+                return Forbid();
+            }
+
+            return Ok(user);
+        }
+    }
+}
--- PortlandAnimalShelterApi/Startup.cs
+++ PortlandAnimalShelterApi/Startup.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using PortlandAnimalShelterApi.Models;
+using PortlandAnimalShelterApi.Helpers;
+using PortlandAnimalShelterApi.Services;
+
 
 namespace PortlandAnimalShelterApi
 {
@@ -26,10 +29,38 @@ namespace PortlandAnimalShelterApi
 
         public void ConfigureServices(IServiceCollection services)
         {
+            // services.AddCors();
             services.AddEntityFrameworkMySql()
                 .AddDbContext<PortlandAnimalShelterApiContext>(options => options
                 .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddScoped<IUserService, UserService>();
+
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -42,6 +73,13 @@ namespace PortlandAnimalShelterApi
             {
                 app.UseHsts();
             }
+
+            // app.UseCors(x => x
+            //     .AllowAnyOrigin()
+            //     .AllowAnyMethod()
+            //     .AllowAnyHeader());
+
+            app.UseAuthentication();
 
             // app.UseHttpsRedirection();
             app.UseMvc();
